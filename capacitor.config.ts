c95111cd30a5f889491f448a0b84a32a0dc96edc
Ales <alesgjeta@gmail.com>
2025-06-02 23:01:36 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'polis-app',
+  webDir: 'www'
+};
+
+export default config;
